138b9f39b209cd9a5a7275e218f1b0efe1f82dd9
davetherpaguy <dave@davetherpaguy.com>
2025-09-07 16:57:23 -0400

    gemini api call working using script argument

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
 def main():
-    print("Hello from aiagent!")
+    if len(sys.argv) != 2:
+        print("Invalid number of arguments. Must provide prompt.")
+        sys.exit(1)
+    
+    prompt_contents = sys.argv[1]
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    model = "gemini-2.0-flash-001"
+
+    response = client.models.generate_content(model=model, contents=prompt_contents)
+
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
