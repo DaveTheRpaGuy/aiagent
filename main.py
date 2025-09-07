51549cffa7dd2793107a7492ffafff7b76cf37fa
davetherpaguy <dave@davetherpaguy.com>
2025-09-07 17:25:31 -0400

    working on Get Files -- not functional as of this commit

--- main.py
+++ main.py
@@ -2,24 +2,37 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print("Invalid number of arguments. Must provide prompt.")
         sys.exit(1)
     
-    prompt_contents = sys.argv[1]
+    user_prompt = sys.argv[1]
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     model = "gemini-2.0-flash-001"
 
-    response = client.models.generate_content(model=model, contents=prompt_contents)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
 
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    response = client.models.generate_content(
+        model=model,
+        contents=messages
+        )
+
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+    print(f"Response text: {response.text}")
+
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
 
 
 if __name__ == "__main__":
